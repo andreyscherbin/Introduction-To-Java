9c90a93fc55a3a70f9c3654cdb7d64c7358e9de5
andreyscherbin <andrey123scherbin@gmail.com>
2020-02-02 15:08:59 +0300

    правка 1.1

--- 1_Basics_of_software_code_development/1.java
+++ 1_Basics_of_software_code_development/1.java
@@ -1,4 +1,4 @@
-package com.company;
+﻿package com.company;
 
 import java.util.Scanner;
 
@@ -8,16 +8,17 @@ public class Main {
      * Найдите значение функции z = ((a-3)*b/2)+c
      */
     public static void main(String[] args) {
-
-	 int a = 0, b = 0, c = 0;
-	 Scanner in = new Scanner(System.in);
-	 System.out.print("Enter the value of the variable a: ");
-	 a = in.nextInt();
+	
+     int a = 0, b = 0, c = 0;
+     Scanner in = new Scanner(System.in);
+     System.out.print("Enter the value of the variable a: ");
+     a = in.nextInt();	
      System.out.print("Enter the value of the variable b: ");
      b = in.nextInt();
      System.out.print("Enter the value of the variable c: ");
      c = in.nextInt();
-	 int z = ((a-3)*b/2)+c;
-	 System.out.printf("The value of the function is: %d \n", z);
+     int z = ((a-3)*b/2)+c;
+     System.out.printf("The value of the function is: %d \n", z);
+	 
     }
 }
