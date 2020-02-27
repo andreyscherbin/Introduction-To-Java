4fb1ac32a066eb93ef1adf6dc11e978e3fd284d1
andreyscherbin <andrey123scherbin@gmail.com>
2020-02-27 14:55:08 +0300

    update

--- 5_Basics_of_OOP/Задача.2/src/com/company/Main.java
+++ 5_Basics_of_OOP/Задача.2/src/com/company/Main.java
@@ -1,10 +1,14 @@
 package com.company;
 
+/*
+    Создать класс Payment с внутренним класом, с помощью объектов которого можно сформировать покупку из
+    нескольких товаров
+*/
 public class Main {
 
     public static void main(String[] args) {
 
-        Payment pay = new Payment("Первая покупка");
+        Payment pay = new Payment("Payment");
         pay.setPayment();
         pay.printCheque();
     }
